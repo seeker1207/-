dad1588c71e3ce28f1b2e5d7d9bd16186d82d8b1
seeker1207 <jm3420@naver.com>
2020-08-26 17:58:13 +0900

    1034번 램프

--- Brute-force/17779번 개리맨더링 2.py
+++ Brute-force/17779번 개리맨더링 2.py
@@ -1,26 +1,41 @@
+from collections import deque
 
-def divide(x, y, d1, d2):
+def make_border(x, y, d1, d2):
+
+    for r in range(1, N+1):
+        for c in range(1, N+1):
+            T[r][c] = 5
+
     # 1번선거구
-    for r in range(1, x+d1):
-        for c in range(1, y+1):
-            T[r][c] = 1
+    for r in range(1, x):
+        T[r][y] = 1
 
     # 2번선거구
-    for c in range(y+1, N+1):
-        for r in range(1, x+d2+1):
-            T[r][c] = 2
+    for c in range(y+d2+1, N+1):
+        T[x+d2][c] = 2
 
     # 3번선거구
-    for c in range(1, y-d1+d2):
-        for r in range(x + d1, N+1):
-            T[r][c] = 3
+    for c in range(1, y-d1):
+        T[x+d1][c] = 3
 
     # 4번 선거구
-    for r in range(x + d2 + 1, N+1):
-        for c in range(y - d1 + d2, N+1):
-            T[r][c] = 4
+    for r in range(x + d1 + d2 + 1, N+1):
+        T[r][y-d1+d2] = 4
 
-    # 5번 선거구
+def indexing(index, start):
+    visited = set()
+    Q = deque([start])
+
+    move_x = [-1, 0, 1, 0]
+    move_y = [0, 1, 0, -1]
+
+    while Q:
+        now = Q.popleft()
+        if now not in visited:
+            visited.add(now)
+            for i in range(4):
+
+                Q.append[(now[0]+move_x[i], now[1]+move_y[i])]
 
 
 N = int(input())
@@ -38,6 +53,7 @@ T.append(['0']*(N+2))
 #         for x in range(1, N - (d1+d2)):
 #             for y in range(1 + d1, N - d2):
 #                 divide(x, y, d1, d2)
-divide(2, 4, 2, 2)
+make_border(2, 5, 3, 2)
+
 for t in T:
     print(t)
