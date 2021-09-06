659472ffb6ab1b035bed7a5046f719393b862492
Ji Hun Son <jm3420@naver.com>
2021-09-06 21:12:09 +0900

    Update 합승 택시 요금.js

--- 카카오 코딩테스트/합승 택시 요금.js
+++ 카카오 코딩테스트/합승 택시 요금.js
@@ -1,20 +1,33 @@
 function solution(n, s, a, b, fares) {
-    var answer = 0;
+    var answer = Infinity;
     const graph = {};
-    let dp = new Array(n+1).fill(new Array(n+1).fill(Infinity));
-    // console.log(dp)
-    for (let i=0; i<n+1; i++) {
-        dp[i][i] = 0;
+    let dp = new Array(n).fill(null).map(() => 
+            new Array(n).fill(null).map(() => Infinity));
+ 
+    for (let i=0; i<n; i++) {
+        for (let j=0; j<n; j++) {
+            if (i == j) {dp[i][i] = 0;}
+        }
     }
-    
+
     fares.forEach((fr) => {
         const [start, end, fare] = fr;
-        dp[start][end] = fare;
-        dp[end][start] = fare;
+        dp[start-1][end-1] = fare;
+        dp[end-1][start-1] = fare;
     })
+
+
+    for (let i=0; i<n; i++) {
+        for (let j=0; j<n; j++) {
+            for (let k=0; k<n; k++) {
+                dp[j][k] = Math.min(dp[j][k], dp[j][i] + dp[i][k])
+            }
+        }
+    }
     
-    console.log(dp)
-    
+    for (let l=0; l<n; l++) {
+        answer = Math.min(answer, dp[s-1][l] + dp[l][a-1] + dp[l][b-1]);
+    }
     
     return answer;
 }
